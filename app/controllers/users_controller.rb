c60cb9790239975e46d79f5be1b95b2ec117b37a
talal2891 <talal.zahid@tkxel.com>
2014-07-01 10:03:12 +0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
     	flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user  #where will it redirect it to ???
     else
       render 'new'
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
    it { should respond_to(:password_digest) }
    it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it {should respond_to(:remember_token)}
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
@@ -22,6 +23,12 @@ describe User do
                        password: " ", password_confirmation: " ")
     end
     it { should_not be_valid }
+  end
+
+
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
   describe "when password doesn't match confirmation" do
